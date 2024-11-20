6b02a55516420597c5ed307609a514bd1e520cd9
Александр Васитенков <kovalevsaska@bk.ru>
2024-11-20 21:26:11 +0300

    Добавьте файлы проекта.

---
+++ FXT.Domain/Entities/CurrencyRate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FXTimeSeries.Domain.Entities;
+
+public class CurrencyRate
+{
+    public Guid Id { get; set; }
+    public DateTime Date { get; set; }
+    public string CurrencyPair { get; set; }
+    public decimal Rate { get; set; }
+}
